a1726a1dadd9d8ca2d921eb3fefd2f6fe4be5ff9
Khorsov <hhorsovv@gmail.com>
2024-01-08 19:42:18 -0800

    searchTest22

--- src/test/java/paymentsTest/step_definition/searchTest.java
+++ src/test/java/paymentsTest/step_definition/searchTest.java
@@ -3,5 +3,6 @@ package paymentsTest.step_definition;
 public class searchTest {
     public static void main(String[] args) {
         System.out.println("SearchTest");
+        System.out.println("Search2");
     }
 }
