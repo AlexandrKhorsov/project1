d7e8ed518804423d70fa1f19a1d388cd41f3c71e
Khorsov <hhorsovv@gmail.com>
2024-01-08 18:44:43 -0800

    last addings22

--- src/test/java/paymentsTest/step_definition/googleTest.java
+++ src/test/java/paymentsTest/step_definition/googleTest.java
@@ -34,7 +34,7 @@ public class googleTest {
     @Then("title should include {string} name")
     public void titleShouldIncludeName(String product) {
         System.out.println("Hi");
-
+        System.out.println("Hi2");
         Assert.assertTrue(Driver.getDriver().getTitle().contains(product));
 
 
