8420dc6e16ca5d35ecf2ed8d9202172d91b813e3
Khorsov <hhorsovv@gmail.com>
2024-01-08 20:41:35 -0800

    add 3rd line

--- src/test/java/paymentsTest/step_definition/searchTest.java
+++ src/test/java/paymentsTest/step_definition/searchTest.java
@@ -4,6 +4,6 @@ public class searchTest {
     public static void main(String[] args) {
         System.out.println("SearchTest");
         System.out.println("Search2");
-
+        System.out.println("Search3");
     }
 }
